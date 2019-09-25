54667da4c10ca597e95a37ee2fdd9e73548ec665
Julien Wajsberg <felash@gmail.com>
2019-09-25 17:47:45 +0200

    Add git and install hooks

---
+++ bin/post-rewrite.js
@@ -0,0 +1,21 @@
+// @noflow
+
+// This is either 'rebase' or 'amend'.
+if (process.env.HUSKY_GIT_PARAMS !== 'rebase') {
+  process.exit();
+}
+
+const checkWarnYarnChanged = require('./check-warn-yarn-changed.js');
+
+const { createInterface } = require('readline');
+
+const rl = createInterface({
+  input: process.stdin,
+});
+
+rl.on('line', line => {
+  const [origHead, head] = line.split(' ');
+  checkWarnYarnChanged(origHead, head).then(
+    changed => changed && process.exit()
+  );
+});
