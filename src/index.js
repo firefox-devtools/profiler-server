f7f925baecb7368ffd9b8837e149e86b3111f94b
Julien Wajsberg <felash@gmail.com>
2019-09-30 16:03:19 +0200

    Extract app creation into its own file

--- src/index.js
+++ src/index.js
@@ -3,16 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @flow
 
-import Koa from 'koa';
-
+import { createApp } from './app';
 import config from './config';
-import routes from './routes';
 
-const app = new Koa();
-
-const router = routes();
-app.use(router.routes());
-app.use(router.allowedMethods());
-
+const app = createApp();
 app.listen(config.httpPort);
 console.log('The HTTP server is listening on port %s.', config.httpPort);
