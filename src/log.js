5da0a5344901ed6eaa97a9429ae7b67209df82e6
Julien Wajsberg <felash@gmail.com>
2019-10-22 12:19:09 +0200

    Add logger util

---
+++ src/log.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// @flow
+//
+// Shamelessly stolen from Firefox Send.
+
+import mozlog from 'mozlog';
+
+import { config } from './config';
+
+const isProduction = config.env === 'production';
+
+export const getLogger = mozlog({
+  app: 'FirefoxProfiler',
+  level: isProduction ? 'INFO' : 'verbose',
+  fmt: isProduction ? 'heka' : 'pretty',
+  debug: !isProduction, // This asserts a correct usage of the library
+});
