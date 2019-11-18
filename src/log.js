d26b5802e16f5154326ca658b3e09586efda6d74
Julien Wajsberg <felash@gmail.com>
2019-11-18 17:59:46 +0100

    Adjust the default log level and make it configurable

--- src/log.js
+++ src/log.js
@@ -7,13 +7,51 @@
 
 import mozlog from 'mozlog';
 
-import { config } from './config';
+type LowerCasedLogLevel =
+  | 'trace' //     This will gather a stack automatically.
+  | 'verbose' //   This is displayed only when run with LOG_LEVEL=verbose
+  | 'debug' //     This is displayed in test and development environments.
+  | 'info' //      This is displayed in production too.
+  | 'warn' //      This is for messages more important than info but that aren't errors.
+  | 'error' //     This is for non-fatal errors.
+  | 'critical'; // This is for fatal errors.
 
-const isProduction = config.env === 'production';
+function toValidLogLevel(logLevel: ?string) {
+  if (!logLevel) {
+    return null;
+  }
+
+  const lowerCasedLogLevel = logLevel.toLowerCase();
+  switch (lowerCasedLogLevel) {
+    case 'trace':
+    case 'verbose':
+    case 'debug':
+    case 'info':
+    case 'warn':
+    case 'error':
+    case 'critical':
+      return lowerCasedLogLevel;
+    default:
+      return null;
+  }
+}
+
+// Note we don't use `config` because we want `config` to be able to log using
+// this file, and we want to avoid cycles in dependencies.
+const isProduction = process.env.NODE_ENV === 'production';
+
+// We can configure the log level from an environment variable LOG_LEVEL, but by
+// default this is 'debug' in development and test, and 'info' in production.
+const logLevelFromEnvironment = toValidLogLevel(process.env.LOG_LEVEL);
+export const logLevel: LowerCasedLogLevel = logLevelFromEnvironment
+  ? logLevelFromEnvironment
+  : isProduction
+  ? 'info'
+  : 'debug';
 
 export const getLogger = mozlog({
   app: 'FirefoxProfiler',
-  level: isProduction ? 'INFO' : 'verbose',
+  level: logLevel,
   fmt: isProduction ? 'heka' : 'pretty',
   debug: !isProduction, // This asserts a correct usage of the library
 });
