2325704dc754dd27dcc160be0b7f4ee32bd82940
Julien Wajsberg <felash@gmail.com>
2020-01-13 17:22:56 +0100

    Configure the middlewares directly using Koa instead of Koa-router so that they're always run

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @flow
 
-import Router from '@koa/router';
 import cors from '@koa/cors';
 
 import { dockerFlowRoutes } from './dockerflow';
@@ -12,16 +11,51 @@ import { cspReportRoutes } from './cspReport';
 
 import { versioning } from '../middlewares';
 
-export function routes() {
-  const router = new Router();
-  router.use(dockerFlowRoutes().routes());
-  router.use(cspReportRoutes().routes());
+import type Koa from 'koa';
 
-  // Versioning and CORS applies only to API routes, that's why we specify it here.
-  router.use(versioning(1));
-  // We use the default configuration for cors, that is we allow all origins and
-  // all methods.
-  router.use(cors());
-  router.use(publishRoutes().routes());
-  return router;
+/**
+ * This function adds the main endpoints for this app.
+ * We pass the app directly instead of returning a router, because koa-router
+ * won't execute middlewares if there's no matching route (see
+ * https://github.com/koajs/router/issues/44) which is a problem for CORS
+ * preflight requests. So we have to specify the CORS middleware directly on the
+ * app object.
+ *
+ * For each router koa-router exposes 2 middlewares:
+ * - the result of routes() returns the configured routes.
+ * - the result of allowedMethods() configures the OPTIONS verb.
+ * We need to add them both if we want that the OPTIONS verb works properly
+ * (besides CORS).
+ */
+export function configureRoutes(app: Koa) {
+  // Adding the main endpoints for this app.
+  // koa-router exposes 2 middlewares:
+  // - the result of routes() returns the configured routes.
+  // - the result of allowedMethods() configures the OPTIONS verb.
+
+  configureTechnicalRoutes(app);
+
+  // Versioning and CORS applies only to API routes, that's why we specify them
+  // here. Also we specify the CORS middleware before the Versioning middleware
+  // so that versioning doesn't apply to CORS preflight requests.
+
+  // Note we use the default configuration for cors, that is we allow all
+  // origins and all methods.
+  app.use(cors());
+  app.use(versioning(1));
+
+  configureUserFacingRoutes(app);
+}
+
+function configureTechnicalRoutes(app: Koa) {
+  const dockerFlow = dockerFlowRoutes();
+  const cspReport = cspReportRoutes();
+
+  app.use(dockerFlow.routes()).use(dockerFlow.allowedMethods());
+  app.use(cspReport.routes()).use(cspReport.allowedMethods());
+}
+
+function configureUserFacingRoutes(app: Koa) {
+  const publish = publishRoutes();
+  app.use(publish.routes()).use(publish.allowedMethods());
 }
