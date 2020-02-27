721557a3ddf921b46511a81549b92cceec55314c
Julien Wajsberg <felash@gmail.com>
2020-02-27 18:40:38 +0100

    Update libdefs

--- src/types/libdef/npm/koa-json-body_v5.x.x.js
+++ src/types/libdef/npm/koa-json-body_v5.x.x.js
@@ -1,4 +1,6 @@
-// @flow
+// flow-typed signature: 2e9661b5e8d01926d949aeea1da6f63e
+// flow-typed version: 6868abec69/koa-json-body_v5.x.x/flow_>=v0.53.x
+
 declare module 'koa-json-body' {
   declare type Middleware = (
     ctx: any,
@@ -6,7 +8,7 @@ declare module 'koa-json-body' {
   ) => Promise<void> | void;
   declare type Options = $Shape<{|
     fallback: boolean,
-    limit: string,
+    limit: string | number,
     strict: boolean,
   |}>;
 
