fad6870951cb37975d22c50120c3a68530a26da1
Brandon Wells <bwells@mozilla.com>
2025-06-03 17:01:06 -0400

    Removed the scripts folder. Older change, not needed

--- scripts/store_git_info.sh
+++
@@ -1,3 +0,0 @@
-# version.json generated from ./bin/generate-version-file.js
-git log -n 1 --no-color --pretty='%s' > ./commit-summary.txt
-git log -n 1 --no-color --pretty=medium > ./commit-description.txt
